7149f38b68a382280ce0f2f9551c4b849c356d66
Alexander Graf <alexander-graf@users.noreply.github.com>
2025-06-10 03:09:12 +0200

    Cleanup: remove ignored files from repo

--- build/.qt/QtDeployTargets-MinSizeRel.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_espanso_helper_FILE C:/Users/crazykungfu/Projects/c++EspansoHelper/build/MinSizeRel/espanso_helper.exe)
-set(__QT_DEPLOY_TARGET_espanso_helper_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_espanso_helper_RUNTIME_DLLS C:/Qt/6.9.1/msvc2022_64/bin/Qt6Widgets.dll;C:/Qt/6.9.1/msvc2022_64/bin/Qt6Gui.dll;C:/Qt/6.9.1/msvc2022_64/bin/Qt6Core.dll)
